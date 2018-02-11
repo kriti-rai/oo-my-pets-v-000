6437e284ce498604185833923c4ed05612573637
Kriti Rai <kriti.rai@gmail.com>
2018-02-11 22:45:19 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,33 @@
 class Owner
-  attr_accessor :pets
+  attr_accessor :pets, :owners
   @pets = {fishes:[], cats:[], dogs:[]}
+  @@owners = []
 
+  #CLASS METHODS
+  
+  def class.owners
+  #keeps track of the owners that have been created
+    @@owners
+  end
+  def class.owners_count
+    owner = self.new
+    self.owners << owner unless self.owners.include?(owner)
+  end
+
+  def initialize(name)
+    #class method to initialize an owner
+    @name = name
+  end
+
+  #INSTANCE METHODS
+  def species(species)
+    class.initialize(species)
+  end
+
+  def say_species
+    #can say its species
+  end
+
+  
+  
 end

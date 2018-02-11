def96f2b2c8aa058945c690386d9db9ec7e699c2
Kriti Rai <kriti.rai@gmail.com>
2018-02-11 22:56:41 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -9,15 +9,15 @@ class Owner
   #keeps track of the owners that have been created
     @@owners
   end
-
+  
   def class.owners_count
     owner = self.new
-    self.owners << owner
+    self.owners << owner unless self.owners.include?(owner)
   end
   # def initialize(name)
     #class method to initialize an owner
-    @name = name
-  end
+  #   @name = name
+  # end
   #
   # #INSTANCE METHODS
   # def species(species)

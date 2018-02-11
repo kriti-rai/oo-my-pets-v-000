3716d3ad7034aaa682d03b0992999923fa756996
Kriti Rai <kriti.rai@gmail.com>
2018-02-11 22:56:57 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -9,10 +9,11 @@ class Owner
   #keeps track of the owners that have been created
     @@owners
   end
-  
+
   def class.owners_count
     owner = self.new
-    self.owners << owner unless self.owners.include?(owner)
+    self.owners << owner
+    #  unless self.owners.include?(owner)
   end
   # def initialize(name)
     #class method to initialize an owner
